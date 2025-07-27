6afc6e8448892b405e3615fdebbbfd663acc6ffd
JonathanMarquez102030 <jmp19731@gmail.com>
2025-07-27 16:32:41 -0600

    Section 10: Copy of filters to ProjectSecurityProdConfig

--- Springsection10/src/main/java/com/eazybytes/config/ProjectSecurityProdConfig.java
+++ Springsection10/src/main/java/com/eazybytes/config/ProjectSecurityProdConfig.java
@@ -2,7 +2,10 @@ package com.eazybytes.config;
 
 import com.eazybytes.exceptionhandling.CustomAccessDeniedHandler;
 import com.eazybytes.exceptionhandling.CustomBasicAuthenticationEntryPoint;
+import com.eazybytes.filter.AuthoritiesLoggingAfterFilter;
+import com.eazybytes.filter.AuthoritiesLoggingAtFilter;
 import com.eazybytes.filter.CsrfCookieFilter;
+import com.eazybytes.filter.RequestValidationBeforeFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,10 +20,8 @@ import org.springframework.security.web.authentication.password.HaveIBeenPwnedRe
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
-import org.springframework.security.web.util.matcher.AnyRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-
 import java.util.Collections;
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -68,6 +69,11 @@ public class ProjectSecurityProdConfig {
         )
         .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class);
 
+    //Adding custom filters
+    http.addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class);
+    http.addFilterAfter(new AuthoritiesLoggingAfterFilter(), BasicAuthenticationFilter.class);
+    http.addFilterAt(new AuthoritiesLoggingAtFilter(), BasicAuthenticationFilter.class);
+
     //Adding session management configuration
     //http.sessionManagement(smc -> smc.invalidSessionUrl("/invalidSession").maximumSessions(1).maxSessionsPreventsLogin(true))
     // .redirectToHttps((https) -> https.requestMatchers(AnyRequestMatcher.INSTANCE))
@@ -79,15 +85,11 @@ public class ProjectSecurityProdConfig {
     //Adding http requests configuration
     http.authorizeHttpRequests((requests) -> requests
         .requestMatchers("/myAccount")
-        //        .requestMatchers("/myAccount").hasAuthority("VIEWACCOUNT") //hasAuthority to access with specific authority
-//        .requestMatchers( "/myBalance").hasAnyAuthority("VIEWBALANCE", "VIEWACCOUNT") //hasAnyAuthority to access with any of the different authorities
-//        .requestMatchers("/myLoans").hasAuthority("VIEWLOANS")
-//        .requestMatchers("/myCards").hasAuthority("VIEWCARDS")
         .hasRole("USER") //hasAuthority to access with specific authority
         .requestMatchers("/myBalance").hasAnyRole("USER",
             "ADMIN") //hasAnyAuthority to access with any of the different authorities
-        .requestMatchers("/myLoans").hasAuthority("USER")
-        .requestMatchers("/myCards").hasAuthority("USER")
+        .requestMatchers("/myLoans").hasRole("USER")
+        .requestMatchers("/myCards").hasRole("USER")
         .requestMatchers("/user").authenticated()
         .requestMatchers("/notices", "/contact", "/error", "/register", "/invalidSession")
         .permitAll());
